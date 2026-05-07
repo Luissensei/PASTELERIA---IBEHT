33ce3d9935fd4e0027b56f5c5d42cc2d2fd74654
Luissensei <cantillol145@gmail.com>
2026-05-07 00:30:16 -0500

    Add files via upload
    Actualizacion Pasteleria - IBEHT - V 1.2

--- PasteleriaSystem/src/pasteleriasystem/PasteleriaSystem.java
+++ PasteleriaSystem/src/pasteleriasystem/PasteleriaSystem.java
@@ -1,9 +1,12 @@
 package pasteleriasystem;
 
-public class PasteleriaSystem {
+import pasteleriasystem.Vistas.Inicio;
 
+public class PasteleriaSystem {
     public static void main(String[] args) {
-         new Vistas.Inicio().setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            Inicio ventana = new Inicio();
+            ventana.setVisible(true);
+        });
     }
-    
 }
